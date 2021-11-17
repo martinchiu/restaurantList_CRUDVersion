0f656eea72b25808cbd8674aa746f1873a125a38
martinchiu <martin87713@gmail.com>
2021-11-17 15:57:32 +0800

    Introduce template engine and Add static files

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.send('This is my movie list built with Express')
+  res.render('index')
+})
+app.get('/restaurants/1', (req, res) => {
+  res.render('show')
 })
 
 // start and listen on the Express server
