dff85c19243c203f0facc178bf141b17b76e00a1
martinchiu <martin87713@gmail.com>
2021-12-01 20:47:46 +0800

    refactor: modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -10,18 +10,10 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < restaurantsData.length; i++) {
-    Restaurant.create({ 
-      name: `${restaurantsData[i].name}`,
-      name_en: `${restaurantsData[i].name_en}`,
-      category: `${restaurantsData[i].category}`,
-      image: `${restaurantsData[i].image}`,
-      location: `${restaurantsData[i].location}`,
-      phone: `${restaurantsData[i].phone}`,
-      google_map: `${restaurantsData[i].google_map}`,
-      rating: `${restaurantsData[i].rating}`,
-      description: `${restaurantsData[i].description}`
+  Restaurant.create(restaurantsData)
+    .then(() => {
+      console.log('done')
+      db.close()
     })
-  }
-  console.log('done')
+    .catch(error => console.log(error))
 })
