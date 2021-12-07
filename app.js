4330d89b103f6be1ae21f9361e37ba374f619481
martinchiu <martin87713@gmail.com>
2021-12-07 19:40:46 +0800

    refactor: modify mongoose and create config

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // require data and packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-
+// 引入 Schema
 const Restaurant = require('./models/Restaurant')
+// 引入 config
+require('./config/mongoose')
 
 const port = 3000
 
@@ -14,19 +15,6 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/restaurantList')
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // setting static files
 app.use(express.static('public'))
@@ -34,6 +22,8 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 // setting methodOverride
 app.use(methodOverride('_method'))
+
+
 
 // routes setting
 // 瀏覽全部餐廳
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,15 +1,9 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../Restaurant')
 const restaurantsData = require('../../restaurant.json').results
 
-mongoose.connect('mongodb://localhost/restaurantList')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 db.once('open', () => {
-  console.log('mongodb connected!')
   Restaurant.create(restaurantsData)
     .then(() => {
       console.log('done')
