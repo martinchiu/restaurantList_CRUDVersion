b4ea76a5634a62ec97a790402358467b0d2f59e9
martinchiu <martin87713@gmail.com>
2022-01-06 09:59:48 +0800

    feat: 新增註冊功能

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // login
 router.get('/login', (req, res) => {
   res.render('login')
@@ -11,6 +13,34 @@ router.post('/login', (req, res) => {
 // register
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', (req, res) => {
+  // 取得註冊表單參數
+  const { email, password, confirmPassword } = req.body
+  const name = req.body.name || 'client'  // 因為 name 非必填，使用者未填時自動賦值
+  
+  // 檢查使用者是否已經註冊
+  User.findOne({ email }).then(user => {
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.render('login', { email })) // 註冊完成導回登入頁，並帶上註冊的 email
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 
