7ea94d30f2177f2a485a3329c5116d5d034dc2f4
martinchiu <martin87713@gmail.com>
2022-01-06 10:06:52 +0800

    feat: 新增express-session ＆ 修改註解

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 // 引入 Schema
 const Restaurant = require('./models/Restaurant')
@@ -18,17 +19,15 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-
-// setting static files
+// use middleware
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
-
-// setting body-parser
 app.use(express.urlencoded({ extended: true }))
-
-// setting methodOverride
 app.use(methodOverride('_method'))
-
-// 將 request 導入路由器
 app.use(routes)
 
 // start and listen on the Express server
